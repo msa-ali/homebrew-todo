5a7978b3127725aba11146fcfe95197d72d6eecb
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2024-01-29 06:44:54 +0530

    update todo homebrew app

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -4,9 +4,9 @@
 class Todo < Formula
     desc "A simple todo list manager for the command line written in Rust"
     homepage "https://github.com/msa-ali/todo-rust"
-    url "https://github.com/msa-ali/todo-rust/releases/download/v0.1.0/todo-mac.tar.gz"
-    sha256 "0ff2d6cb1d91afa350e5d8528323479af2d6741b30fba8e37b9fd0bd85c5b3c1"
-    version "0.1.0"
+    url "https://github.com/msa-ali/todo-rust/releases/download/v0.2.0/todo-mac.tar.gz"
+    sha256 "b121168807e3379607f49064b94d07008162faefc751f6e01b70c763082c2733"
+    version "0.2.0"
   
     def install
       bin.install "todo"
